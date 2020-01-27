c90a39bf1b27ab45db548af2c2870e3957fd5823
Lewis Holland Clarke <lewishollandclarke@Lewiss-MacBook-Pro.local>
2020-01-27 09:06:17 +0000

    Inital Commit

---
+++ src/test/java/com/lewis/dockerApi/DockerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewis.dockerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
